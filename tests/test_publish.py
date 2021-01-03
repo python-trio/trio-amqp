4ce0650cb0fc4992f577ca5f56cb0a7354328771
Matthias Urlichs <matthias@urlichs.de>
2021-01-03 15:40:37 +0100

    fix test_publish and remove merge mess
    8fd5a6ebee638581d49b80b765ca3cd122161e6d

--- tests/test_publish.py
+++ tests/test_publish.py
@@ -62,24 +62,27 @@ class TestPublish(testcase.RabbitTestCase):
     @pytest.mark.skip("This callback doesn't exist in asyncamqp")
     @pytest.mark.trio
     async def test_return_from_publish(self, channel):
-        called = False
+        called = trio.Event()
 
-        async def callback(channel, body, envelope, properties):
-            nonlocal called
-            called = True
-        channel.return_callback = callback
+        async def logger(task_status=trio.TASK_STATUS_IGNORED):
+            task_status.started()
+            async for a,b,c in channel:
+                called.set()
 
-        # declare
-        await channel.exchange_declare("e", "topic")
+        async def sender(task_status=trio.TASK_STATUS_IGNORED):
+            task_status.started()
 
-        # publish
-        await channel.publish(b"coucou", "e", routing_key="not.found",
-                                   mandatory=True)
+            # declare
+            await channel.exchange_declare("e", "topic")
 
-        for i in range(10):
-            if called:
-                break
-            await anyio.sleep(0.1)
-
-        assert called
-
+            # publish
+            await channel.publish("coucou", "e", routing_key="not.found",
+                                  mandatory=True)
+ 
+        with trio.fail_after(1):
+            async with trio.open_nursery() as n:
+                await n.start(logger)
+                await n.start(sender)
+                await called.wait()
+                n.cancel_scope.cancel()
+ 
